ab6154b863bd9c8bf0acde68130a3924a2b1500b
baejungmo <bjm1244>
2021-12-11 08:59:23 +0900

    FIX : api POST로 수정

--- main/views.py
+++ main/views.py
@@ -13,17 +13,19 @@ def index(request):
 
 
 def get_show_variable(request):
-    variable_list = show_variable_all()
-    logger.info("system variable list : {}".format(variable_list))
-    return JsonResponse({'data': variable_list}, status=200)
+    if request.method == 'POST':
+        variable_list = show_variable_all()
+        logger.info("system variable list : {}".format(variable_list))
+        return JsonResponse({'data': variable_list}, status=200)
 
 
 def get_time(request):
-    now_time = datetime.datetime.now()
-    now_date_list = [{
-        "variable_idx": 0,
-        "variable_name": "current time",
-        "value": now_time
-    }]
-    logger.info("now date list : {}".format(now_date_list))
-    return JsonResponse({'data': now_date_list}, status=200)
+    if request.method == 'POST':
+        now_time = datetime.datetime.now()
+        now_date_list = [{
+            "variable_idx": 0,
+            "variable_name": "current time",
+            "value": now_time
+        }]
+        logger.info("now date list : {}".format(now_date_list))
+        return JsonResponse({'data': now_date_list}, status=200)
