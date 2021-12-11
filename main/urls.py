c1498b6d50344db3d10591a7c10ff4a09144f755
baejungmo <bjm1244>
2021-12-11 10:00:08 +0900

    FIX : api 주소 변경

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_variable_all/', views.get_show_variable, name='variable_all'),
-    path('get_time/', views.get_time, name='current_time'),
+    path('get_variable_all', views.get_show_variable, name='variable_all'),
+    path('get_time', views.get_time, name='current_time'),
 ]
